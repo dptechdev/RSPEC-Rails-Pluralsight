b7de2556e8342a8b9009a78ef3732802bfedec81
Derek Pauley <dpauleytechnology@gmail.com>
2015-01-30 16:55:39 -0600

    Initital Commit
    Initial Commit

---
+++ CommentsDashboard/spec/lib/wordpress/comments/client_spec.rb
@@ -0,0 +1,47 @@
+#require 'nokogiri'
+
+module Wordpress
+	module Comments 
+		class Client
+
+			attr_reader :url
+
+			def initialize url
+				@url = url
+			end
+
+			def parse xml 
+				#doc = Nokogiri::XML xml 
+				#doc.search('item')
+			end
+		end
+	end
+end
+
+describe Wordpress::Comments::Client do 
+
+	describe "#initialize" do 
+
+	it "stores a URL" do
+		client = Wordpress::Comments::Client.new 'http://mashable.com/category/rss/comments/feed/'
+		
+
+		expect(client.url).to eq 'http://mashable.com/category/rss/comments/feed/'
+	end
+
+	end
+
+	describe "#parse" do
+
+		let(:xml) {File.read(File.join('spec', 'fixtures', 'feed.xml')) }
+
+		it "extracts the link" do
+			client = Wordpress::Comments::Client.new 'http://mashable.com/category/rss/comments/feed/'
+			comments = client.parse xml
+			comment = comments.first
+			link = 'http://mashable.com/2012/07/18/ipad-early-photos/comment-page-1/#comment-18239503'
+			expect(comment.link).to eq link
+		end
+	end
+
+end
